6c58f34de2c0311a9811618e175231073fd656c6
soumyajeet <soumyajeet@soumyajeet-HP-Notebook>
2019-06-25 19:28:36 +0530

    Commit Debut

---
+++ consumerBank/src/main/java/banking/consumerBank/App.java
@@ -0,0 +1,13 @@
+package banking.consumerBank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World2!" );
+    }
+}
